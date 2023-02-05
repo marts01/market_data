23115c12cd8ed28dd3ef51d528e21ede6419acfa
marts01 <mart@mlconcepts.net>
2023-02-05 14:34:40 +0800

    Add files via upload

---
+++ market_data_get_func.py
@@ -0,0 +1,93 @@
+# Add API KEY to the Token Header or import from a .py file
+# Get A Series of Historical Option Quotes from the API
+
+# Example usage:
+# get_options_quote_from('IBM', '2023-02-10', 'C', '140', '2023-01-01')
+
+import requests
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import api_key
+
+def get_options_chainbyExpr(symbol, expiry_date):
+    # mf.get_options_chainbyExpr('SPXW', '2023-02-17')
+
+    url = 'https://api.marketdata.app/v1/options/chain/'
+    
+    #expiry_date = datetime.strptime(expiry_date, '%Y-%m-%d')
+    #expiry_date = expiry_date.strftime('%y%m%d')
+    date_format = 'timestamp'
+    headers = {'Authorization': api_key.API_KEY}
+    
+
+    path = f'{symbol}/?exiration={expiry_date}&dateformat=timestamp'
+
+    final_url = url + path
+    chain_expr = requests.get(final_url, headers=headers)
+
+    return chain_expr.text
+
+def get_options_chainbyMonth(symbol):
+    # mf.get_options_chainbyMonth('SPX')
+    #https://api.marketdata.app/v1/options/chain/AAPL/?monthly=true&dateformat=timestamp
+    
+    url = 'https://api.marketdata.app/v1/options/chain/'  #AAPL/?monthly=true&dateformat=timestamp
+    
+    
+    headers = {'Authorization': api_key.API_KEY}
+
+    path = f'{symbol}/?monthly=true&dateformat=timestamp'
+
+    final_url = url + path
+    chain_expr = requests.get(final_url, headers=headers)
+    return chain_expr.text
+
+
+
+def get_hist_quotes_to_from(symbol_list, from_date, to_date):
+    # mf.get_hist_quotes_to_from(spx_list, '2023-01-26', '2023-02-04')
+    
+    #https://api.marketdata.app/v1/options/quotes/AAPL250117C00150000/?from=2023-01-01&to=2023-01-31&dateformat=timestamp
+    
+    url = 'https://api.marketdata.app/v1/options/quotes/'
+    
+    headers = {'Authorization': api_key.API_KEY}
+
+    symbols = symbol_list
+
+    # Initialize an empty DataFrame to hold the data
+    df = pd.DataFrame()
+
+    # Iterate through the symbols, making a request for each one
+    for symbol in symbols:
+        # Build the path by concatenating the symbol, date and date_format
+        path = f'{symbol}/?from={from_date}&to={to_date}&dateformat=timestamp'
+        # Concatenate the base URL and the path
+        final_url = url + path
+        # Make the request
+        response = requests.get(final_url, headers=headers)
+        # Extract the data from the response
+        data = response.json()
+        # Create a new DataFrame with the data and a column for the symbol
+        symbol_df = pd.DataFrame({'updated': data.get('updated', np.nan),
+                         'symbol': symbol,
+                         'bid': data.get('bid', np.nan),
+                         'bidSize': data.get('bidSize', np.nan),
+                         'mid': data.get('mid', np.nan),
+                         'ask': data.get('ask',np.nan),
+                         'askSize': data.get('askSize', np.nan),
+                         'last': data.get('last', np.nan),
+                         'openInterest': data.get('openInterest', np.nan),
+                         'volume': data.get('volume', np.nan),
+                         'inTheMoney': data.get('inTheMoney', np.nan),
+                         'intrinsicValue': data.get('intrinsicValue', np.nan),
+                         'extrinsicValue': data.get('extrinsicValue', np.nan),
+                         'underlyingPrice': data.get('underlyingPrice', np.nan)})
+
+
+        # Add a new column to symbol_df with the id and date values concatenated
+        symbol_df['id'] = symbol + '_' + symbol_df['updated'].astype(str)
+        # Use the new column as the index when concatenating with df
+        df = pd.concat([df, symbol_df.set_index('id')], ignore_index=False, sort=False, axis=0)
+    return df
